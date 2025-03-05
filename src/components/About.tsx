9ab45274021fb3dd88a244a2953ffdad4353f574
sank-8 <sanket.unifins@gmail.com>
2025-03-05 17:04:42 +0530

    spell fix

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
         <section id="about">
             <h2 className="text-xl font-bold">About</h2>
             <p className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert pt-2">
-                I am 1 third-year BTech student in Computer Science and Engineering, graduating in 2026.
+                I am third-year BTech student in Computer Science and Engineering, graduating in 2026.
                 I have over a year of experience in full-stack development using the MERN stack.
                 Currently, I am exploring Web3, Blockchain and focusing on improving my problem-solving skills.
                 I have worked on various projects and internships,
